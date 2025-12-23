da4ff75f2d5499d1b2c61eff01560f4ffc17e5ad
SBabchukTM <babchuk.serhii@trident-media.agency>
2025-12-23 10:58:05 +0200

    init

---
+++ OneMoreDraw/App/OneMoreDrawApp.swift
@@ -0,0 +1,17 @@
+//
+//  OneMoreDrawApp.swift
+//  OneMoreDraw
+//
+//  Created by Serhii Babchuk on 23.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct OneMoreDrawApp: App {
+    var body: some Scene {
+        WindowGroup {
+            OMLaunchView()
+        }
+    }
+}
